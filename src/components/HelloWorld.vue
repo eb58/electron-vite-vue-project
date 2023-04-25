94029c870f4b29cf239cb265478fd06e730afaa4
Erich Brandl <erich.brandl@atos.net>
2023-04-25 16:08:42 +0200

    add ffi-napi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,48 @@
 <script>
-// const ffi = require("ffi-napi");
-// const ref = require("ref-napi");
+const ref = require("ref-napi");
+const ffi = require("ffi-napi");
 
-const winax = require("./winax/winax-for-electron-15.5.7/winax");
-//const winax = require("./winax/winax-for-electron-16.2.8/winax");
+const voidPtr = ref.refType(ref.types.void);
+const stringPtr = ref.refType(ref.types.CString);
+
+const winax = require("./winax/winax-for-electron-16.2.8/winax");
+// const winax = require("./winax/winax-for-electron-15.5.7/winax");
 //const winax = require("./winax/winax-for-electron-17.4.11/winax");
 //const winax = require("./winax/winax-for-electron-22.3.6/winax");
 // const winax = require("./winax/winax-for-electron-24.1.2/winax");
 // const winax = require("winax");
 
-// console.log("WINAX", winax);
-// console.log("REF", ref);
-// console.log("FFi", ffi);
+const user32 = ffi.Library("user32.dll", {
+    EnumWindows: ["bool", [voidPtr, "int32"]],
+    EnumDesktopWindows: ["bool", ["long", voidPtr, "int32"]],
+    GetWindowTextA: ["long", ["long", stringPtr, "long"]],
+    SetForegroundWindow: ["bool", ["long"]],
+    SetFocus: ["long", ["long"]],
+    ShowWindow: ["bool", ["long", "int32"]],
+    BringWindowToTop: ["bool", ["long"]],
+});
+
+const findWindow = (s) => {
+    let n = 0;
+    let res = false;
+    const windowProc = ffi.Callback("bool", ["long", "int32"], (hWnd) => {
+        if (!res) {
+          const buf = Buffer.alloc(255, " ");
+          user32.GetWindowTextA(hWnd, buf, 255);
+          const x = buf.toString().trim();
+          if (x) {
+            n++;
+            // console.log(x, x.length, buf);
+            res = res || x.includes(s);
+          }
+        }
+    });
+    user32.EnumWindows(windowProc, 0);
+    console.log("end", res, n);
+    return res;
+};
+
+console.log("WINAX", winax, ffi, ref, user32);
 
 export default {
     methods: {
@@ -24,9 +55,9 @@ export default {
             }
         },
         testWinax() {
-            console.log("WINAX", winax);
-            const app = new winax.Object("Word.Application");
-            app.visible = true;
+            // const app = new winax.Object("Word.Application"); app.visible = true;
+            const x = findWindow("neu 1 - Notepad++");
+            console.log( x );
         },
     },
 };
